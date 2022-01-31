20bb8e94f4bc351ecf9bb6e0b8b95641a0f77512
Judy <judy.mtz1@gmail.com>
2022-01-31 00:50:14 -0600

    created taskEl() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,24 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    //create list item
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+var createTaskEl = function(taskDataObj) {
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
 
-    // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" 
-    + taskNameInput 
-    + "</h3><span class='task-type'>"
-    +taskTypeInput + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-};
+}
 
 formEl.addEventListener("submit", taskFormHandler);
