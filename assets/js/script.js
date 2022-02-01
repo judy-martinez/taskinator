98add319df956abd9046f88e07915d93e3d2a32c
Judy <judy.mtz1@gmail.com>
2022-01-31 22:45:40 -0600

    added reset method for form element

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,8 @@ var taskFormHandler = function(event) {
         return false;
     }
 
+    formEl.reset();
+    
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
