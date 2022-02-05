99c20b70f131cbaaf68f33ca89fe27be9030fe71
Judy <judy.mtz1@gmail.com>
2022-02-04 23:22:29 -0600

    updated code comments and spacing in taskStatusChangeHandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,11 +124,13 @@ var taskButtonHandler = function(event) {
 
     // edit button was clicked
     if (targetEl.matches(".edit-btn")) {
+        console.log("edit", targetEl);
         var taskId = targetEl.attribute("data-task-id");
         editTask(taskId);
     }
     // delete button was clicked
     else if (targetEl.matches(".delete-btn")) {
+        console.log("delete", targetEl);
         // get the element's task id
         var taskId = targetEl.getAttribute("data-task-id");
         deleteTask(taskId);
@@ -137,23 +139,18 @@ var taskButtonHandler = function(event) {
 
 var taskStatusChangeHandler = function(event) {
     console.log(event.target.value);
-
-    //get the task item's id
+    // find task list item based on event.target's data-task-id attribute
     var taskId = event.target.getAttribute("data-task-id");
-
+    //find the parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     //get the currently selected option's value and convert to lowercase
     var statusValue = event.target.value.toLowerCase();
 
-    //find the parent task item element based on the id
-    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
-
     if (statusValue === "to do") {
         tasksToDoEl.appendChild(taskSelected);
-    }
-    else if (statusValue === "in progress") {
+    } else if (statusValue === "in progress") {
         tasksInProgressEl.appendChild(taskSelected);
-    }
-    else if (statusValue === "completed") {
+    } else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
     }
 };
